047c414f8f1d8dcbe03fa5afc734608c4bc4d242
Omar Kraidié <omppula_1@hotmail.com>
2024-01-09 18:03:12 +0200

    Clean up.

--- components/PeriodicTable/TableGaps.tsx
+++ components/PeriodicTable/TableGaps.tsx
@@ -9,17 +9,9 @@ const TableGaps = ({}: TableGapsProps) => {
 	const BerylliumBoronGap = <div className="col-span-10 col-start-4 row-start-3" />;
 	const MagnesiumAluminiumGap = <div className="col-span-10 col-start-4 row-start-4" />;
 
-	const ElementColors: { [key in IElementType]: string } = {
-		'alkali metal': 'bg-lime-200 dark:bg-lime-950 ',
-		metalloid: 'bg-yellow-200 dark:bg-yellow-950 ',
-		actinide: 'bg-orange-200 dark:bg-orange-950 ',
-		'alkali earth metal': 'bg-red-200 dark:bg-red-950 ',
-		'reactive non-metal': 'bg-blue-200 dark:bg-blue-950 ',
-		'unknown type': 'bg-gray-200 dark:bg-gray-800 ',
-		'transition metal': 'bg-violet-200 dark:bg-violet-950 ',
-		'noble gas': 'bg-fuchsia-200 dark:bg-fuchsia-950 ',
-		'post-transition metal': 'bg-teal-200 dark:bg-teal-950 ',
-		lanthanide: 'bg-sky-200 dark:bg-sky-950 ',
+	const ElementColors: Pick<{ [key in IElementType]: string }, 'actinide' | 'lanthanide'> = {
+		actinide: 'bg-orange-200 dark:bg-orange-950',
+		lanthanide: 'bg-sky-200 dark:bg-sky-950',
 	};
 
 	// Spans the empty space that is allocated for the lanthanide and actinide elements seperator.
