a930bb98e07abfe7d00c0a376a1e790a099d0662
Omar Kraidié <omppula_1@hotmail.com>
2024-01-13 23:47:29 +0200

    Update layout and page overflow to use "auto" instead of "scroll"

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,3 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return <div className="container min-h-screen pt-[64.8px] overflow-scroll">{children}</div>;
+	return <div className="container min-h-screen pt-[64.8px] overflow-auto">{children}</div>;
 }
